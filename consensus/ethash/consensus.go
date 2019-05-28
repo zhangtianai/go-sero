c969593785dc01ae3c9b1dbe504d3fd21757c681
gordon-sero <gordon@sero.vip>
2019-05-28 17:00:58 +0800

    fix params for sip3

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -288,9 +288,6 @@ func calcDifficultyAutumnTwilight(time uint64, parent *types.Header) *big.Int {
 	if x.Cmp(params.MinimumDifficulty) < 0 {
 		x.Set(params.MinimumDifficulty)
 	}
-	if parent.Number.Uint64()+1 == cpt.SIP3 {
-		x.Exp(big.NewInt(10), big.NewInt(9), nil)
-	}
 	return x
 }
 
